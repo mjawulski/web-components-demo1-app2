a31b517bb12afcbe872afd22fda667787016ddae
Michał Jawulski <mjawulski@gmail.com>
2019-02-28 23:37:46 +0100

    target result

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
-// import { CookiesInfoComponent } from './cookies-info/cookies-info.component';
+import { CookiesInfoComponent } from "./cookies-info/cookies-info.component";
 
 @NgModule({
-  declarations: [AppComponent /*, CookiesInfoComponent*/],
+  declarations: [AppComponent, CookiesInfoComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cookies-info/cookies-info.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cookies-info',
+  templateUrl: './cookies-info.component.html',
+  styleUrls: ['./cookies-info.component.css']
+})
+export class CookiesInfoComponent implements OnInit {
+  isClosed: boolean;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
